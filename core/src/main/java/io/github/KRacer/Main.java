e6e0ab4232c32c4093634a06ca3cb1ba49c5dddf
jerian-tgg <jerian.palencia@gmail.com>
2024-12-13 14:04:12 +0800

    Added motor1

--- core/src/main/java/io/github/KRacer/Main.java
+++ core/src/main/java/io/github/KRacer/Main.java
@@ -1,36 +1,63 @@
 package io.github.KRacer;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.graphics.Color;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main implements ApplicationListener {
+
+    Texture motor1Texture;
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+
     @Override
     public void create() {
-        // Prepare your application here.
+        motor1Texture = new Texture("motor1.png");
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(12.8f, 10.8f);
     }
 
     @Override
     public void resize(int width, int height) {
-        // Resize your application here. The parameters represent the new window size.
+        viewport.update(width, height, true);
     }
 
     @Override
     public void render() {
-        // Draw your application here.
+        input();
+        logic();
+        draw();
+    }
+
+    private void input() {
+        // Handle input logic here
+    }
+
+    private void logic() {
+        // Handle game logic here
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+        spriteBatch.draw(motor1Texture, 0, 0, 1, 2);
+        spriteBatch.end();
     }
 
     @Override
-    public void pause() {
-        // Invoked when your application is paused.
-    }
+    public void pause() {}
 
     @Override
-    public void resume() {
-        // Invoked when your application is resumed after pause.
-    }
+    public void resume() {}
 
     @Override
     public void dispose() {
-        // Destroy application's resources here.
+        motor1Texture.dispose();
+        spriteBatch.dispose();
     }
 }
